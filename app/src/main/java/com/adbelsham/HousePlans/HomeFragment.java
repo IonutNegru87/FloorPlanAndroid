6fbbf57cc32fc7ca2ea6c231c57c3c9733609190
Rohit Garg <rohit.garg@imarkinfotech.com>
2016-06-09 19:07:26 +0530

    Update the SeekBar

--- app/src/main/java/com/adbelsham/HousePlans/HomeFragment.java
+++ app/src/main/java/com/adbelsham/HousePlans/HomeFragment.java
@@ -7,12 +7,20 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 
 public class HomeFragment extends Fragment {
+
+    @InjectView(R.id.bedroomSeekBar)
+    SeekBar bedRoomSeekBar;
+
+    @InjectView(R.id.bedroomTextView)
+    TextView bedRoomTextView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,30 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View mView = inflater.inflate(R.layout.fragment_home, container, false);
         ButterKnife.inject(this, mView);
+        bedRoomSeekBar.setOnSeekBarChangeListener(new SeekBarListener());
         return mView;
+    }
+
+    public class SeekBarListener implements SeekBar.OnSeekBarChangeListener {
+        int progress = 0;
+
+        @Override
+
+        public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+            progress = progresValue;
+            bedRoomTextView.setText("Bedrooms " + progresValue);
+        }
+
+        @Override
+
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
     }
 }
